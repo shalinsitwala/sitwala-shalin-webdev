98c488e83bf2cf52ca2a582521a7b6be3c54d563
shalinsitwala <shalin.sitwala@gmail.com>
2017-02-14 01:06:03 -0500

    widget service except updateWidget

--- public/assignment/services/widget.service.client.js
+++ public/assignment/services/widget.service.client.js
@@ -17,10 +17,97 @@
         ];
         var api = {
             "findAllWidgets": findAllWidgets,
-            "findWidgetById": findWidgetById
+            "findWidgetById": findWidgetById,
+            "createWidget" : createWidget,
+            "findWidgetsByPageId": findWidgetsByPageId,
+            "updateWidget": updateWidget,
+            "deleteWidget": deleteWidget
         };
         return api;
+        
+        
+        function deleteWidget(widgetId) {
+            for(var w in widgets){
+                if(widgets[w]._id === widgetId){
+                    widgets[w].splice(w,1);
+                    break;
+                }
+            }
+            return widgets;
+            
+        }
+        function updateWidget(widgetId, widget) {
+            for(var w in widgets){
+                if(widgets[w]._id === widgetId){
+                    // TODO
+                }
+            }
+            
+        }
 
+        function findWidgetsByPageId(pageId) {
+            var rwidgets = [];
+            for(w in widgets){
+                if(widgets[w].pageId === pageId){
+                    rwidgets.push(widgets[w]);
+                }
+            }
+            return rwidgets;
+        }
+        
+        
+        function createWidget(pageId, widget) {
+            if(widget.type === "HEADER"){
+                var newWidget = {
+                    _id: getWidgetId(),
+                    widgetType: widget.type,
+                    pageId: pageId,
+                    size: widget.size,
+                    text: widget.text
+                };
+            }
+            else if (widget.type === "IMAGE" || widget.type === "YOUTUBE"){
+                var newWidget = {
+                    _id: getWidgetId(),
+                    widgetType: widget.type,
+                    pageId: pageId,
+                    width: widget.width,
+                    url: widget.url
+                };
+
+            }
+            else if (widget.type === "HTML"){
+                var newWidget = {
+                    _id: getWidgetId(),
+                    widgetType: widget.type,
+                    pageId: pageId,
+                    text: widget.text
+                };
+
+            }
+            widgets.push(newWidget);
+            return newWidget;
+        }
+
+        function getWidgetId() {
+            var date = new Date();
+
+            var components = [
+                date.getYear(),
+                date.getMonth(),
+                date.getDate(),
+                date.getHours(),
+                date.getMinutes(),
+                date.getSeconds(),
+                date.getMilliseconds()
+            ];
+
+            var id = components.join("");
+
+            return id;
+        }
+        
+        
         function findWidgetById(widgetId) {
             for(var w in widgets){
                 if(widgets[w]._id === widgetId){
